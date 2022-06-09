1dbd388101f3cdacf565411972220f0d4e3e100d
Norman-Zvenyika <noz224@lehigh.edu>
2022-06-09 00:44:02 +0000

    Change Servlets button to Random Quotes

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -13,7 +13,7 @@
 // limitations under the License.
 
 /**
- * Adds a random greeting to the page.
+ * Collapsible Buttons.
  */
 var coll = document.getElementsByClassName("collapsibleTitle");
 var i;
